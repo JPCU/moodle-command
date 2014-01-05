44e489961ab63f5d0fd3e3e3724e28b91aabb52e
Kevin Meziere <kevinmeziere@users.noreply.github.com>
2014-01-04 18:01:57 -0800

    Update backup.sh

--- backup.sh
+++ backup.sh
@@ -3,10 +3,10 @@
 cd $1
 tar -cpf moodlebackup.tar.gz moodledata/ htdocs/
 
-DB_SERVER=$(php -r 'error_reporting(0); define("CLI_SCRIPT", 1); include_once("$1/htdocs/config.php"); echo $CFG->dbhost;')
-DB_NAME=$(php -r 'error_reporting(0); define("CLI_SCRIPT", 1); include_once("$1/htdocs/config.php"); echo $CFG->dbname;')
-DB_USER=$(php -r 'error_reporting(0); define("CLI_SCRIPT", 1); include_once("$1/htdocs/config.php"); echo $CFG->dbuser;')
-DB_PASS=$(php -r 'error_reporting(0); define("CLI_SCRIPT", 1); include_once("$1/htdocs/config.php"); echo $CFG->dbpass;')
+DB_SERVER=$(php -r 'error_reporting(0); define("CLI_SCRIPT", 1); include_once($argv[1]); echo $CFG->dbhost;' ${1}/htdocs/config.php)
+DB_NAME=$(php -r 'error_reporting(0); define("CLI_SCRIPT", 1); include_once($argv[1]); echo $CFG->dbname;' ${1}/htdocs/config.php)
+DB_USER=$(php -r 'error_reporting(0); define("CLI_SCRIPT", 1); include_once($argv[1]); echo $CFG->dbuser;' ${1}/htdocs/config.php)
+DB_PASS=$(php -r 'error_reporting(0); define("CLI_SCRIPT", 1); include_once($argv[1]); echo $CFG->dbpass;' ${1}/htdocs/config.php)
 TIMESTAMP=$(date +%s)
 
 echo "$DB_SERVER:5432:$DB_NAME:$DB_USER:$DB_PASS" > ~/.pgpass
