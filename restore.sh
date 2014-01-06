7278311b19d2029eee348a4e6ebf21ec8b44cf02
Kevin Meziere <kevinmeziere@users.noreply.github.com>
2014-01-05 22:09:23 -0800

    Update restore.sh

--- restore.sh
+++ restore.sh
@@ -3,11 +3,16 @@
 tar -xvf $1 -C / $2
 tar -xvf $1 -C / $3
 
+cp ${2}/config.php ${2}/config.php.tmp
+
+awk '!/require/' ${2}/config.php > ${2}/config.php
+
 DB_SERVER=$(php -r 'error_reporting(0); define("CLI_SCRIPT", 1); include_once($argv[1]); echo $CFG->dbhost;' ${2}/config.php)
 DB_NAME=$(php -r 'error_reporting(0); define("CLI_SCRIPT", 1); include_once($argv[1]); echo $CFG->dbname;' ${2}/config.php)
 DB_USER=$(php -r 'error_reporting(0); define("CLI_SCRIPT", 1); include_once($argv[1]); echo $CFG->dbuser;' ${2}/config.php)
 DB_PASS=$(php -r 'error_reporting(0); define("CLI_SCRIPT", 1); include_once($argv[1]); echo $CFG->dbpass;' ${2}/config.php)
 
+cp ${2}/config.php.tmp ${2}/config.php
 
 printf "\n\nCREATE USER %s WITH PASSWORD '%s';\n" "$DB_USER" "$DB_PASS"
 ##echo CREATE USER $DB_USER WITH PASSWORD \'$DB_PASS\';
