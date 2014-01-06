688ec8f04d81534916965c4dcd4088151988d7fa
Kevin Meziere <kevinmeziere@users.noreply.github.com>
2014-01-05 22:20:11 -0800

    Update restore.sh

--- restore.sh
+++ restore.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 printf "Restoring Moodle code directory... \n"
-tar -xf $1 -C / $2
+tar -xPf $1 -C / $2
 printf "Restoring Moodle data directory... \n"
-tar -xf $1 -C / $3
+tar -xPf $1 -C / $3
 
 printf "Loading Moodle config... \n"
 mv ${2}/config.php ${2}/config.php.orig
