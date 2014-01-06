e5db4ccf45b123273d6824b8c2d82880288cf90c
Kevin Meziere <kevinmeziere@users.noreply.github.com>
2014-01-05 22:13:00 -0800

    Update restore.sh

--- restore.sh
+++ restore.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
+printf "Restoring Moodle code directory... \n"
 tar -xvf $1 -C / $2
+printf "Restoring Moodle data directory... \n"
 tar -xvf $1 -C / $3
 
+printf "Loading Moodle config... \n"
 cp ${2}/config.php ${2}/config.php.tmp
 
 awk '!/require/' ${2}/config.php > ${2}/config.php
@@ -12,13 +15,14 @@ DB_NAME=$(php -r 'error_reporting(0); define("CLI_SCRIPT", 1); include_once($arg
 DB_USER=$(php -r 'error_reporting(0); define("CLI_SCRIPT", 1); include_once($argv[1]); echo $CFG->dbuser;' ${2}/config.php)
 DB_PASS=$(php -r 'error_reporting(0); define("CLI_SCRIPT", 1); include_once($argv[1]); echo $CFG->dbpass;' ${2}/config.php)
 
-cp ${2}/config.php.tmp ${2}/config.php
+#cp ${2}/config.php.tmp ${2}/config.php
 
 printf "\n\nCREATE USER %s WITH PASSWORD '%s';\n" "$DB_USER" "$DB_PASS"
 ##echo CREATE USER $DB_USER WITH PASSWORD \'$DB_PASS\';
-printf "echo create database %s owner %s;\n\n" "$DB_NAME" "$DB_USER"
+printf "create database %s owner %s;\n\n" "$DB_NAME" "$DB_USER"
 ##echo create database $DB_NAME owner $DB_USER;
 
+printf "Unpackaging Moodle database...\n"
 tar -xvf $1 moodlebackup-latest.sql
 tar -xvf $1 $(readlink moodlebackup-latest.sql)
  
@@ -26,7 +30,7 @@ tar -xvf $1 $(readlink moodlebackup-latest.sql)
 echo "$DB_SERVER:5432:$DB_NAME:$DB_USER:$DB_PASS" > ~/.pgpass
 chmod 600 ~/.pgpass
 
-echo 'Restoring database...'
+printf "Restoring database...\n"
 
 #pg_dump -C -h $DB_SERVER -U $DB_USER $DB_NAME -f moodlebackup-$TIMESTAMP.sql
 
