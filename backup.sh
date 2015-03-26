6940c7cc893a9888193a839f70421246802eee09
Kevin Meziere <kevinmeziere@users.noreply.github.com>
2015-03-26 12:53:45 -0700

    Update backup.sh

--- backup.sh
+++ backup.sh
@@ -8,7 +8,7 @@ DB_SERVER=$(php -r 'error_reporting(0); define("CLI_SCRIPT", 1); include_once($a
 DB_NAME=$(php -r 'error_reporting(0); define("CLI_SCRIPT", 1); include_once($argv[1]); echo $CFG->dbname;' ${1}/htdocs/config.php)
 DB_USER=$(php -r 'error_reporting(0); define("CLI_SCRIPT", 1); include_once($argv[1]); echo $CFG->dbuser;' ${1}/htdocs/config.php)
 DB_PASS=$(php -r 'error_reporting(0); define("CLI_SCRIPT", 1); include_once($argv[1]); echo $CFG->dbpass;' ${1}/htdocs/config.php)
-TIMESTAMP=$(date +%s)
+TIMESTAMP="$(date +%s)"
 
 echo "$DB_SERVER:5432:$DB_NAME:$DB_USER:$DB_PASS" > ~/.pgpass
 chmod 600 ~/.pgpass
